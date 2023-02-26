29705970a1df697ecd5e3aee69d8774b3a94609c
marcelvishnk <64962335+marcelvishnk@users.noreply.github.com>
2023-02-26 10:02:53 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-skkrrtpackage com.example.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
